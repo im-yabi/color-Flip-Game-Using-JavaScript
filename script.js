7d8b10f86f62e1d93603895d71551de632775d66
Yabesh Murgan <151531028+im-yabi@users.noreply.github.com>
2025-05-14 23:57:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,239 +1,90 @@
-// variables
-const generalBtn = document.getElementById("genral");
-const businessBtn = document.getElementById("business");
-const sportsBtn = document.getElementById("sport");
-const entertainmentBtn = document.getElementById("entertainment");
-const technologyBtn = document.getElementById("technology");
-const searchBtn = document.getElementById("searchBtn");
+// Getting and Setting the elements and colors needed
 
-const newsQuery = document.getElementById("newsQuery");
-const newsType = document.getElementById("newsType");
-const newsdetails = document.getElementById("newsdetails");
+const boxes = document.querySelector(".boxes");
+const colors = ["aqua","red","blueviolet","chartreuse","coral","gold","green","hotpink"];
+const colorList = [...colors,...colors];
+// console.log(colorList);
+const boxLength = colorList.length;
+// console.log(boxLength);
 
-// Array
-var newsDataArr = [];
+//Initializing the main elements of the Game state
 
-// apis 
-const API_KEY = "f9e0ce1e323042eda598324bfc9ff78b";
-const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
-const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
-const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
-const SPORTS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=";
-const ENTERTAINMENT_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=";
-const TECHNOLOGY_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=8&apiKey=";
-const SEARCH_NEWS = "https://newsapi.org/v2/everything?q=";
-
-window.onload = function() {
-    newsType.innerHTML="<h4>Headlines</h4>";
-    fetchHeadlines();
-};
+let revealCount = 0;
+let activeBox = null;
+let waitingTime = false;
 
 
-generalBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>General news</h4>";
-    fetchGeneralNews();
-});
 
-businessBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Business</h4>";
-    fetchBusinessNews();
-});
+//Function to display the boxes in the webpage
+function buildBoxes(color){
+    const element = document.createElement("div");
+    element.classList.add("box");
+    element.setAttribute("data-color",color); //used for mapping the color
+    element.setAttribute("data-revealed","false");//used to check revelaed boxes
 
-sportsBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Sports</h4>";
-    fetchSportsNews();
-});
+    //adding event listerners for click event
+    element.addEventListener("click",()=>{
+        const revealed = element.getAttribute("data-revealed");
+        //display the boxes
+        if(waitingTime || revealed ==="true" || element == activeBox){
+            return;
+        }
+        element.style.backgroundColor = color;
 
-entertainmentBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Entertainment</h4>";
-    fetchEntertainmentNews();
-});
-
-technologyBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Technology</h4>";
-    fetchTechnologyNews();
-});
-
-searchBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Search : "+newsQuery.value+"</h4>";
-    fetchQueryNews();
-});
-
-const fetchHeadlines = async () => {
-    const response = await fetch(HEADLINES_NEWS+API_KEY);
-    newsDataArr = [];
-    if(response.status >=200 && response.status < 300) {
-        const myJson = await response.json();
-        newsDataArr = myJson.articles;
-    } else {
-        // handle errors
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
-
-    displayNews();
-}
+        //checking the active box
+        if(!activeBox){
+            activeBox = element;
+            return;
+        }
 
 
-const fetchGeneralNews = async () => {
-    const response = await fetch(GENERAL_NEWS+API_KEY);
-    newsDataArr = [];
-    if(response.status >=200 && response.status < 300) {
-        const myJson = await response.json();
-        newsDataArr = myJson.articles;
-    } else {
-        // handle errors
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
+    //Logic for Matching Color and winning condition
+    const colorMatch = activeBox.getAttribute("data-color")
+        if (colorMatch ===color){
+            activeBox.setAttribute("data-revealed","true");
+            activeBox.setAttribute("data-revealed","true");
+            waitingTime=false;
+            activeBox=null;
+            waitingTime=false;
+            revealCount +=2;
+            if(revealCount===boxLength){
+                alert("congratulations...you Won!🤩🪄Refresh to play again...");
+            }
+            return;
+        }
 
-    displayNews();
-}
 
-const fetchBusinessNews = async () => {
-    const response = await fetch(BUSINESS_NEWS+API_KEY);
-    newsDataArr = [];
-    if(response.status >=200 && response.status < 300) {
-        const myJson = await response.json();
-        newsDataArr = myJson.articles;
-    } else {
-        // handle errors
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
 
-    displayNews();
-}
 
-const fetchEntertainmentNews = async () => {
-    const response = await fetch(ENTERTAINMENT_NEWS+API_KEY);
-    newsDataArr = [];
-    if(response.status >=200 && response.status < 300) {
-        const myJson = await response.json();
-        console.log(myJson);
-        newsDataArr = myJson.articles;
-    } else {
-        // handle errors
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
+        //change the waiting time to true and using settimeout for transition
+        waitingTime = true;
+        setTimeout(()=>{
+            element.style.backgroundColor=null;
+            activeBox.style.backgroundColor=null;
+            waitingTime=null;
+            activeBox=null;
+        },1000);
 
-    displayNews();
-}
 
-const fetchSportsNews = async () => {
-    const response = await fetch(SPORTS_NEWS+API_KEY);
-    newsDataArr = [];
-    if(response.status >=200 && response.status < 300) {
-        const myJson = await response.json();
-        newsDataArr = myJson.articles;
-    } else {
-        // handle errors
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
-
-    displayNews();
-}
-
-const fetchTechnologyNews = async () => {
-    const response = await fetch(TECHNOLOGY_NEWS+API_KEY);
-    newsDataArr = [];
-    if(response.status >=200 && response.status < 300) {
-        const myJson = await response.json();
-        newsDataArr = myJson.articles;
-    } else {
-        // handle errors
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
-
-    displayNews();
-}
-
-const fetchQueryNews = async () => {
-
-    if(newsQuery.value == null)
-        return;
-
-    const response = await fetch(SEARCH_NEWS+encodeURIComponent(newsQuery.value)+"&apiKey="+API_KEY);
-    newsDataArr = [];
-    if(response.status >= 200 && response.status < 300) {
-        const myJson = await response.json();
-        newsDataArr = myJson.articles;
-    } else {
-        //error handle
-        console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
-
-    displayNews();
-}
-
-function displayNews() {
-
-    newsdetails.innerHTML = "";
-
-    if(newsDataArr.length == 0) {
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
-        return;
-    }
-
-    newsDataArr.forEach(news => {
-
-        var date = news.publishedAt.split("T");
-        var time = date[1].slice(0, -1);
-        
-        var col = document.createElement('div');
-        col.className="col-sm-12 col-md-4 col-lg-3 p-2 card";
-
-        var card = document.createElement('div');
-        card.className = "p-2";
-
-        var image = document.createElement('img');
-        image.setAttribute("height","matchparent");
-        image.setAttribute("width","100%");
-        image.setAttribute("alt","Image Error")
-        image.src=news.urlToImage;
-
-        var cardBody = document.createElement('div');
-        
-        var newsHeading = document.createElement('h5');
-        newsHeading.className = "card-title";
-        newsHeading.innerHTML = news.title;
-
-        var dateHeading = document.createElement('h6');
-        dateHeading.className = "text-primary";
-        dateHeading.innerHTML = date[0] + " " +time;
-
-        var discription = document.createElement('p');
-        discription.className="text-muted";
-        discription.innerHTML = news.description;
-
-        var link = document.createElement('a');
-        link.className="btn btn-dark";
-        link.setAttribute("target", "_blank");
-        link.href = news.url;
-        link.innerHTML="Read more";
-
-        cardBody.appendChild(newsHeading);
-        cardBody.appendChild(dateHeading);
-        cardBody.appendChild(discription);
-        cardBody.appendChild(link);
-
-        card.appendChild(image);
-        card.appendChild(cardBody);
-
-        col.appendChild(card);
-
-        newsdetails.appendChild(col);
     });
 
+    return element;
+}
+
+
+
+
+
+//Building the Boxes for the game
+for (let i=0; i<boxLength; i++){
+    //this is the place where the colors are randomly displayed
+    const randomIndex = Math.floor(Math.random() * colorList.length);
+    const color = colorList[randomIndex];
+    const box = buildBoxes(color);
+
+    //using splice method to avoid 3 repated calls
+    colorList.splice(randomIndex,1);
+    
+    //console.log(color);
+    boxes.append(box);
 }
